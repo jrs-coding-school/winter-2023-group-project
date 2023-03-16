368648ebba29b4434d46030141790ed67006cf35
Amin-JRS <120671622+Amin-JRS@users.noreply.github.com>
2023-03-16 13:30:10 -0400

    Revert "11 create user profile page" (#39)

--- ui/src/views/Home/index.js
+++ ui/src/views/Home/index.js
@@ -1,11 +1,7 @@
 import React from "react"
-import { utilityJSON } from "../../utility/utils"
 
 function Home (props) {
-
-// console.log(utilityJSON())
-
-  return <div>This is my home test</div>
+  return <div>Home</div>
 }
 
 export default Home
