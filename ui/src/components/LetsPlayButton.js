be9b57c93d9a988a3acf2469e6fc796a74a1b871
Addie Pasok <addiepasok8@gmail.com>
2023-03-17 15:13:27 -0400

    feat: let's play button

---
+++ ui/src/components/LetsPlayButton.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Fragment } from 'react'
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom';
+import { Typography } from '@mui/material';
+
+function LetsPlayButton() {
+  return (
+    <Fragment>
+      <Box>
+        <Button 
+          component={Link} 
+          to='/gamemodes' 
+          variant='contained' 
+          size='large' 
+          sx={{boxShadow:'3px 2px 7px rgb(0, 0, 0, 0.5)'}}>
+          <Typography fontWeight='bold' fontSize='24px'>
+            let's play!
+          </Typography>
+        </Button>
+      </Box>
+    </Fragment>
+  )
+}
+
+export default LetsPlayButton
