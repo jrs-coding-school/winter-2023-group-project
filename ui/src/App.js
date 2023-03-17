9d3a882f9ae7d35bb0f185e54f4fb1c15c050416
Addie <121542304+im-addie@users.noreply.github.com>
2023-03-17 10:48:31 -0400

    2 create account login page (#30)
    * created login form
    
    * added login page
    
    * added sign up button and styling
    
    * fixed styling
    
    * issue #2 - create login page (#40)
    
    * Init User Profile Page
    
    * DB schema creation and seeding, json util function
    
    * REVERT: removed changes dealing with knex schema and seeding
    
    * REFACTOR: abstracted api request
    
    * FEAT: added mock api response for getUserProfile
    
    * FIX: conditional rendering and flex direction
    
    * 28 create knex database schema (#32)
    
    * DB schema creation and seeding, json util function
    
    * Init User Profile Page
    
    * CHG: moved helper function to api
    
    * FIX: comma dangle
    
    * Revert "Init User Profile Page"
    
    This reverts commit afd730f30f75bd824369721adb1fe6ac443d99a8.
    
    * REVERT: removed unrelated api changes
    
    * REMOVED: unnecessary package-lock.json
    
    * bcryprt password for user
    
    * abstracted json questions data
    
    ---------
    
    Co-authored-by: Amin Yachnes <amin@harborec.com>
    
    * Revert "11 create user profile page" (#39)
    
    ---------
    
    Co-authored-by: Damaris Torrent <damaristorrent@gmail.com>
    Co-authored-by: Amin Yachnes <amin@harborec.com>
    Co-authored-by: Amin-JRS <120671622+Amin-JRS@users.noreply.github.com>
    
    * FIX: users will now have their token in their localStorage after logging in
    
    ---------
    
    Co-authored-by: Damaris Torrent <damaristorrent@gmail.com>
    Co-authored-by: Amin Yachnes <amin@harborec.com>
    Co-authored-by: Amin-JRS <120671622+Amin-JRS@users.noreply.github.com>

--- ui/src/App.js
+++ ui/src/App.js
@@ -5,6 +5,7 @@ import Home from './views/Home'
 import NotFound from './views/NotFound'
 import Layout from './layouts'
 import ThemeContextProvider from './context/themeContext'
+import Login from './views/Login'
 import RegisterForm from './views/Register'
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<Layout/>}>
             <Route index element={<Home/>}/>
             <Route path="/about" element={<About/>}/>
+            <Route path="/login" element={<Login/>}/>
             <Route path="*" element={<NotFound/>}/>
             <Route path="/register" element={<RegisterForm/>}/>
           </Route>
