beb3d201c79a6b57de28f4d7a9f8a7b0d6fc039c
Addie Pasok <addiepasok8@gmail.com>
2023-03-17 15:14:24 -0400

    refactored code

--- ui/src/components/LetsPlayButton.js
+++ ui/src/components/LetsPlayButton.js
@@ -8,18 +8,16 @@ import { Typography } from '@mui/material';
 function LetsPlayButton() {
   return (
     <Fragment>
-      <Box>
-        <Button 
-          component={Link} 
-          to='/gamemodes' 
-          variant='contained' 
-          size='large' 
-          sx={{boxShadow:'3px 2px 7px rgb(0, 0, 0, 0.5)'}}>
-          <Typography fontWeight='bold' fontSize='24px'>
-            let's play!
-          </Typography>
-        </Button>
-      </Box>
+      <Button 
+        component={Link} 
+        to='/gamemodes' 
+        variant='contained' 
+        size='large' 
+        sx={{boxShadow:'3px 2px 7px rgb(0, 0, 0, 0.5)'}}>
+        <Typography fontWeight='bold' fontSize='24px'>
+          let's play!
+        </Typography>
+      </Button>
     </Fragment>
   )
 }
