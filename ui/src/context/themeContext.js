13138172acb0226b7ad2ad8489f365304b5637d0
dantewanders <dante@thetridentexperience.com>
2023-03-23 17:25:44 -0400

    Fix: themes

--- ui/src/context/themeContext.js
+++ ui/src/context/themeContext.js
@@ -4,10 +4,32 @@ const theme = createTheme({
   palette: {
     mode: 'light',
     primary: {
-      main: '#1976d2',
+      main: '#8e6176',
+      light: '#ad9393',
+      dark: '#312929'
     },
     secondary: {
-      main: '#f50057',
+      main: '#96c0d4',
+    },
+    info: {
+      main: '#052d46',
+    },
+    background: {
+      default: '#312929',
+      paper: '#bfd2d2',
+    },
+  },
+  typography: {
+    htmlFontSize: 14,
+    button: {
+      fontSize: 15,
+    },
+  },
+
+  typography: {
+    htmlFontSize: 14,
+    button: {
+      fontSize: 15,
     },
   },
 })
