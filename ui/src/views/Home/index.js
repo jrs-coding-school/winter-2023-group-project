8c02ba9205488d705e494a2c9028d07f3270e5d8
Addie Pasok <addiepasok8@gmail.com>
2023-03-23 21:39:11 -0400

    fixed layout and added styling to homepage

--- ui/src/views/Home/index.js
+++ ui/src/views/Home/index.js
@@ -1,95 +1,224 @@
-import React from "react";
-import { Grid, Typography } from "@mui/material";
-import Button from "@mui/material/Button";
-import "../../App.css";
+import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
-import { Box } from "@mui/system";
+import { Grid, Typography } from "@mui/material";
+import ListItem from '@mui/material/ListItem';
 import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
+import Box from '@mui/material/Box';
 import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
+import LetsPlayButton from "../../components/LetsPlayButton"
+import './index.css'
 
-const Item = (props) => (
-  <Grid item xs={props.xs}>
-    {props.children}
-  </Grid>
-);
 function Home(props) {
   return (
-    <Grid container spacing={2}>
-      <Box>
-        <Grid item xs={8}>
-          <Item>
-            <Typography variant="h1" component="div" gutterBottom />
-            <Link to="/GameMode">
-              <Button variant="contained">Quick Play</Button>
-            </Link>
-          </Item>
-        </Grid>
-        <Grid item xs={4}>
-          <Item>
-            <Typography variant="h1" component="div" gutterBottom />
-          </Item>
+
+    <Fragment>
+
+      <Grid m='50px'>
+
+        <Grid item>
+          <Typography variant="h5" fontWeight='bold'>
+            About Trivia Genius
+          </Typography>
+
+          <Typography variant="body1">
+            Trivia Genius is a game in which players are asked multiple choice
+            questions about different topics and they have to get as many correct
+            answers as possible before the timer ends.
+          </Typography>
         </Grid>
 
-        <h2> What Is Trivia Genius And Why You Should Play</h2>
-        <p>
-          Trivia Genius is a game in which players are asked multiple choice
-          questions about different topics and they have to get as many correct
-          answers as possible before the timer ends.
-        </p>
+        <Grid item>
+          <Typography variant="h5" fontWeight='bold' mt='20px'>
+            Benefits of playing Trivia Genius
+          </Typography>
 
-        <h4>Benefits of playing Trivia Genius</h4>
-        <p>
-          Unfortunately every year we grow a bit older and we realize that our
-          mind is not as sharp and fast as it used to be. If we don't train our
-          brain, it gets harder for us to remember, concentrate and learn new
-          skills and information. Quiz questions can help prevent this process
-          to go so fast as it serves as a form of exercise for our brain
-        </p>
-        <h4>Trivia Genius "The Game"</h4>
-        <p>
-          Trivia Genius offers 10 different categories such as Art, Sports,
-          Society & Culture, History, Science, Geography, Film/TV, Music, Food &
-          Drink and General Knowledge. We offer three different Game Modes.
-          Quick Play which is a three minute game of a topic of your choice. 3
-          Strikes which sets the timer and you must answer as many questions as
-          possible before you answer three incorrectlt. Finally, Fast 25. This
-          is where you are timed to answer how long it takes you to answer 25
-          questions correctly. Ech game mode offers three difficulty settings.
-          Easy, Medium and Hard. So, if you still haven't decided to become a
-          Trivia Genius. Jump into our Quick Play and enjoy a three minute
-          General Knowledge multiple choice Trivia teaser
-        </p>
-        <p> Here is a sample question from our Music Category:</p>
-        <p> "Which English rock band released the song 'Us and Them'?"</p>
-        <p>
-          {" "}
-          A) Pink Floyd, B) Wings, C) Rudimental, D) The Alan Parsons Project{" "}
-        </p>
-        <Card sx={{ maxWidth: 345 }}>
-          <CardMedia
-            sx={{ height: 140 }}
-            image="../../assets/trivia_image.jpg"
-            title="Game Screenshot"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              Trivia Genius
+          <Typography variant="body1">
+            Every year, we grow a bit older and we realize that our
+            mind is not as sharp and fast as it used to be. If we don't train our
+            brain, it gets harder for us to remember, concentrate and learn new
+            skills and information. Quiz questions can help prevent this process
+            to go so fast as it serves as a form of exercise for our brain.
+          </Typography>
+        </Grid>
+
+        <Grid item>
+          <Typography variant="h5" fontWeight='bold' mt='20px'>
+            About the Game
+          </Typography>
+
+            {/* put a table of categories instead of just listing them out */}
+            {/* list difficulties */}
+
+            <Typography variant="body1" fontWeight='bold' fontSize='18px' mt='3px'>
+              Categories:
             </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Example of the Trivia Genius Quick Play Game
+
+            <Box width='400px'>
+
+              <Grid container 
+                rowSpacing={1} 
+                columnSpacing={{ xs: 1 }}
+                mt='3px'
+              >
+                
+                <Grid item xs={6}>
+                  <ListItem sx={{ display: 'list-item' }}>
+                    Arts & Literature
+                  </ListItem>
+                </Grid>
+
+                <Grid item xs={6}>
+                  <ListItem sx={{ display: 'list-item' }}>
+                    Film & TV
+                  </ListItem>
+                </Grid>
+                
+                <Grid item xs={6}>
+                  <ListItem sx={{ display: 'list-item' }}>
+                    Food & Drink
+                  </ListItem>
+                </Grid>
+                
+                <Grid item xs={6}>
+                  <ListItem sx={{ display: 'list-item' }}>
+                    General Knowledge
+                  </ListItem>
+                </Grid>
+                
+                <Grid item xs={6}>
+                  <ListItem sx={{ display: 'list-item' }}>
+                    Geography
+                  </ListItem>
+                </Grid>
+                
+                <Grid item xs={6}>
+                  <ListItem sx={{ display: 'list-item' }}>
+                    History
+                  </ListItem>
+                </Grid>
+                
+                <Grid item xs={6}>
+                  <ListItem sx={{ display: 'list-item' }}>
+                    Music
+                  </ListItem>
+                </Grid>
+                
+                <Grid item xs={6}>
+                  <ListItem sx={{ display: 'list-item' }}>
+                    Science
+                  </ListItem>
+                </Grid>
+                
+                <Grid item xs={6}>
+                  <ListItem sx={{ display: 'list-item' }}>
+                    Society & Culture
+                  </ListItem>
+                </Grid>
+                
+                <Grid item xs={6}>
+                  <ListItem sx={{ display: 'list-item' }}>
+                    Sport & Leisure
+                  </ListItem>
+                </Grid>
+
+              </Grid>
+            
+            </Box>
+
+            <Typography variant="body1" fontWeight='bold' fontSize='18px' mt='20px'>
+              Game Modes:
             </Typography>
-          </CardContent>
-          <CardActions>
-            <Button size="small">Learn More</Button>
-          </CardActions>
-        </Card>
-        <Link to="/GameMode">
-          <Button variant="contained">Quick Play</Button>
-        </Link>
-      </Box>
-    </Grid>
+
+            <Box width='800px' mt='10px' ml='20px'>
+
+              <Grid item xs={12}>
+                <Typography variant="body1" fontWeight='bold' fontSize='16px' mt='3px'>
+                  Quick Play
+                </Typography>
+    
+                <ListItem>
+                  Three minutes of answering trivia questions in a category of your choice.
+                </ListItem>
+              </Grid>
+
+              <Grid item xs={12}>
+                <Typography variant="body1" fontWeight='bold' fontSize='16px' mt='3px'>
+                  3 Strikes
+                </Typography>
+                
+                <ListItem>
+                  Answer as many questions as possible before you answer three incorrectly.
+                </ListItem>
+              </Grid>
+              
+              <Grid item xs={12}>
+                <Typography variant="body1" fontWeight='bold' fontSize='16px' mt='3px'>
+                  Fast 25
+                </Typography>
+
+                <ListItem>
+                  Race against the clock and answer 25 questions correctly.
+                </ListItem>
+              </Grid>
+              
+            </Box>
+
+            <Typography component={Link} to='/how-to-play' variant="subtitle" color='grey' className="link">More details of each game mode</Typography>
+
+            <Typography variant="body1" fontWeight='bold' fontSize='18px' mt='20px'>
+              Difficulty settings:
+            </Typography>
+
+            <Box width='800px' mt='10px' ml='20px'>
+
+              <Grid item xs={12}>
+                <ListItem sx={{ display: 'list-item' }}>
+                  Easy
+                </ListItem>
+    
+              </Grid>
+
+              <Grid item xs={12}>
+                <ListItem sx={{ display: 'list-item' }}>
+                 Medium
+                </ListItem>
+                
+              </Grid>
+              
+              <Grid item xs={12}>
+                <ListItem sx={{ display: 'list-item' }}>
+                  Hard
+                </ListItem>
+
+              </Grid>
+              
+            </Box>
+
+        </Grid>
+
+        {/* insert screenshot of gameplay here! */}
+
+        {/* 
+          question: Which English rock band released the song "Us and Them"?
+          options: Pink Floyd, Wings, Rudimental, The Alan Parsons Project 
+        */}
+
+        <Grid
+          container
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Typography variant="h5" mt='30px' mb="15px">
+            Ready to become a Trivia Genius?
+          </Typography>
+        
+          <LetsPlayButton />
+        </Grid>
+
+      </Grid>
+
+    </Fragment>
   );
 }
 
